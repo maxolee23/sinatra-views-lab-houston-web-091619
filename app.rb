0fcf1b98c67639c3190385e5043a62d5be023f38
Your Name <you@example.com>
2019-10-07 14:23:23 -0500

    done.

--- app.rb
+++ app.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye 
+	end 
+
+	get '/date' do 
+		@date = DateTime.new(2019,10,7,2,11,5)
+		erb :date 
+	end
+
 
 end
